d0f28d848038734aa6aafc03d87b5c884c714106
Aylia <aylia.sabadie@student.junia.com>
2025-02-03 19:39:15 +0100

    Test de la fonction trouverNote effectué.

--- TP2/tests.c
+++ TP2/tests.c
@@ -30,9 +30,42 @@ static char* test_creerNote() {
 	mu_assert("Assert failed creerNote(): la note est mal créée\n", note->frequence == 40 && note->duree==5);
 }
 
+static char* test_trouverNote_enregistree() {
+	Note** test = creerTableau(12);
+	float frequencies[12] = { 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88 };
+	for (int i = 0; i < 12; i++) {
+		test[i] = creerNote(frequencies[i], 300);
+	}
+	int retour = trouverNote(test,'a');
+	mu_assert("Assert failed trouverNote(): la touche est enregistree mais ne retourne pas la bonne note\n", retour == 1);
+}
+
+static char* test_trouverNote_non_enregistree() {
+	Note** test = creerTableau(12);
+	float frequencies[12] = { 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88 };
+	for (int i = 0; i < 12; i++) {
+		test[i] = creerNote(frequencies[i], 300);
+	}
+	int retour = trouverNote(test, 'v');
+	mu_assert("Assert failed trouverNote(): la touche n'est pas enregistree mais la fonction ne renvoie pas le 1 pour continuer\n", retour == 1);
+}
+
+static char* test_trouverNote_quitter() {
+	Note** test = creerTableau(12);
+	float frequencies[12] = { 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88 };
+	for (int i = 0; i < 12; i++) {
+		test[i] = creerNote(frequencies[i], 300);
+	}
+	int retour = trouverNote(test, 'q');
+	mu_assert("Assert failed trouverNote(): Le q ne permet pas de quitter le simulateur de piano\n", retour == 0);
+}
+
 static char* all_tests() {
 	mu_run_test(test_creerTableau);
 	mu_run_test(test_creerNote);
+	mu_run_test(test_trouverNote_enregistree);
+	mu_run_test(test_trouverNote_non_enregistree);
+	mu_run_test(test_trouverNote_quitter);
 
 	return 0;
 }
